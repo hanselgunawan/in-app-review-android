10ee16f65ff007c97e7b26538525dcd60d3cd954
Hansel Tritama <hansel.tritama@nortonlifelock.com>
2022-03-16 16:10:05 -0700

    Adding in-app review dialog

--- app/src/main/java/com/example/inappreview/MainActivity.kt
+++ app/src/main/java/com/example/inappreview/MainActivity.kt
@@ -2,10 +2,52 @@ package com.example.inappreview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.inappreview.databinding.ActivityMainBinding
+import com.google.android.play.core.review.ReviewInfo
+import com.google.android.play.core.review.ReviewManager
+import com.google.android.play.core.review.ReviewManagerFactory
+import com.google.android.play.core.tasks.Task
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var reviewInfo: ReviewInfo
+    private lateinit var manager: ReviewManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        activateReviewInfo()
+
+        binding.inAppReviewButton.setOnClickListener {
+            startReviewFlow()
+        }
+    }
+
+    private fun activateReviewInfo() {
+        manager = ReviewManagerFactory.create(this)
+
+        // it will return a task review info
+        // this should be called at the correct time
+        val managerInfoTask: Task<ReviewInfo> = manager.requestReviewFlow()
+
+        managerInfoTask.addOnCompleteListener { task ->
+            if (task.isSuccessful) {
+                reviewInfo = task.result
+            } else {
+                Toast.makeText(this, "Review failed to start", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun startReviewFlow() {
+        val flow: Task<Void> = manager.launchReviewFlow(this, reviewInfo)
+        flow.addOnCompleteListener { task ->
+            Toast.makeText(this, "Rating is completed", Toast.LENGTH_SHORT).show()
+        }
     }
 }
